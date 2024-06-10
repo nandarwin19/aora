5211ea7e1691b68ea6a1545b637a46027623479b
nwin19 <nandarwinbuma@gmail.com>
2024-06-10 21:32:05 +0630

    file and folder Structure

--- app/index.jsx
+++ app/index.jsx
@@ -8,7 +8,7 @@ export default function App() {
     <View className="flex-1 items-center justify-center bg-white">
       <Text className="text-3xl font-pbold">Open</Text>
       <StatusBar style="auto" />
-      <Link href="/profile" style={{ color: "blue" }}>
+      <Link href="/home" style={{ color: "blue" }}>
         Go to
       </Link>
     </View>
