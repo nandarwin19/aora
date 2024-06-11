f4f1317aa0c12c1f62fc8cc6f7305d5e6c1f4ea4
nwin19 <nandarwinbuma@gmail.com>
2024-06-11 11:35:40 +0630

    auth screens

--- app/index.jsx
+++ app/index.jsx
@@ -1,4 +1,4 @@
-import { Link } from "expo-router";
+import { Link, router } from "expo-router";
 import { StatusBar } from "expo-status-bar";
 import React from "react";
 import {
@@ -16,7 +16,7 @@ export default function App() {
   return (
     <SafeAreaView className="bg-primary h-full">
       <ScrollView contentContainerStyle={{ height: "100%" }}>
-        <View className="w-full justify-center items-center h-full px-4">
+        <View className="w-full justify-center items-center min-h-[85vh] px-4">
           <Image
             source={images.logo}
             className="w-[130px] h-[84px]"
