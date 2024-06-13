48952148d8cfdd927198095fa438899fb69c57b8
nwin19 <nandarwinbuma@gmail.com>
2024-06-13 10:03:10 +0630

    Trending video section

--- app/(tabs)/home.jsx
+++ app/(tabs)/home.jsx
@@ -13,7 +13,7 @@ import { images } from "../../constants";
 import SearchInput from "../../components/SearchInput";
 import Trending from "../../components/Trending";
 import EmptyState from "../../components/EmptyState";
-import { getAllPosts } from "../../lib/appwrite";
+import { getAllPosts, getLatestPosts } from "../../lib/appwrite";
 import useAppwrite from "../../lib/useAppwrite";
 import VideoCard from "../../components/VideoCard";
 // import useAppwrite from "../../lib/useAppwrite";
@@ -22,6 +22,7 @@ import VideoCard from "../../components/VideoCard";
 const Home = () => {
   const [refreshing, setRefreshing] = useState(false);
   const { data: posts, refetch } = useAppwrite(getAllPosts);
+  const { data: latestPosts } = useAppwrite(getLatestPosts);
 
   const onRefresh = async () => {
     setRefreshing(true);
@@ -73,8 +74,7 @@ const Home = () => {
                 Latest Videos
               </Text>
 
-              
-              <Trending posts={[{ id: 1 }, { id: 2 }, { id: 3 }] ?? []} />
+              <Trending posts={latestPosts ?? []} />
             </View>
           </View>
         )}
